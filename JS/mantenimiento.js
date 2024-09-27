fa21626d1ec4b3655c025ca5a073faf9d30f4def
DanielEC16 <danielcairo695@gmail.com>
2024-09-27 18:33:16 -0500

    pagina casi cien por ciento

--- JS/mantenimiento.js
+++ JS/mantenimiento.js
@@ -30,3 +30,63 @@ btnMenu.addEventListener("click", () => {
       }, false)
     })
   })()
+
+  // Selecciona todos los enlaces del menú
+const menuLinks = document.querySelectorAll('.ds-menu ul li a');
+
+// Selecciona todos los contenidos
+const contents = document.querySelectorAll('.content');
+
+// Función para mostrar el contenido basado en el data-target
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault(); // Previene la acción por defecto del enlace
+        
+        // Remueve la clase 'active' de todos los enlaces y contenidos
+        menuLinks.forEach(link => link.classList.remove('active'));
+        contents.forEach(content => content.classList.remove('active'));
+        
+        // Añade la clase 'active' al enlace y al contenido correspondiente
+        link.classList.add('active');
+        const target = link.getAttribute('data-target');
+        document.getElementById(target).classList.add('active');
+    });
+});
+
+   // Selecciona todos los inputs de archivo
+   const inputFiles = document.querySelectorAll('input[type="file"]');
+
+   // Itera sobre cada input de archivo
+   inputFiles.forEach(inputFile => {
+     const pictureImage = inputFile.previousElementSibling.querySelector(".picture__image");
+     const containerImg = inputFile.previousElementSibling;
+
+     // Establece el texto por defecto
+     pictureImage.innerHTML = "Seleccionar Imagen";
+
+     inputFile.addEventListener("change", function (e) {
+       const inputTarget = e.target;
+       const file = inputTarget.files[0];
+
+       if (file) {
+         const reader = new FileReader();
+
+         reader.addEventListener("load", function (e) {
+           const readerTarget = e.target;
+
+           const img = document.createElement("img");
+           img.src = readerTarget.result;
+           img.classList.add("picture__img", "img-fluid");
+
+           pictureImage.innerHTML = ""; // Limpia el texto
+           pictureImage.appendChild(img); // Añade la imagen
+           containerImg.classList.remove("p-3"); // Quita el padding
+         });
+
+         reader.readAsDataURL(file);
+       } else {
+         containerImg.classList.add("p-3"); // Vuelve a agregar el padding
+         pictureImage.innerHTML = "Choose an image"; // Restaura el texto por defecto
+       }
+     });
+   });
